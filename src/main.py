afefcf693b3acc07aa4f3eb0f5a80b2141b1cb1f
SOURAV CHAKRABORTY_25 <souravjhebd@gmail.com>
2026-02-27 22:39:45 +0600

    Add multiply function to calculator

--- src/main.py
+++ src/main.py
@@ -7,3 +7,6 @@ print("Today's date:", date.today())
 
 print("Add:", add(10, 5))
 print("Subtract:", subtract(10, 5))
+from utils import add, subtract, multiply
+# ...
+print("Multiply:", multiply(10, 5))
